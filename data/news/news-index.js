9e0f4990e433da2f3416967028b4bfc0046198f8
Lukasz Madej <lukasz.madej@gmail.com>
2026-08-12 10:57:41 +0200

    Fix news tag styling

--- data/news/news-index.js
+++ data/news/news-index.js
@@ -4,70 +4,70 @@ window.newsIndex = [
     "title": "Przegląd Otwocki pisze o przebudowie i naszej petycji",
     "date": "2026-08-11",
     "excerpt": "Kolejny lokalny portal, Przegląd Otwocki, opisał szczegóły przebudowy ul. Żeromskiego oraz naszą inicjatywę i petycję mieszkańców.",
-    "tags": ["prasa"]
+    "tags": ["prasa", "inicjatywa"]
   },
   {
     "slug": "pisza-o-nas-portal-otwocki",
     "title": "Nasza inicjatywa w lokalnych mediach",
     "date": "2026-08-11",
     "excerpt": "Nasza inicjatywa została zauważona przez lokalne media. Na portalu PortalOtwocki.pl ukazał się artykuł poświęcony remontowi ul. Żeromskiego.",
-    "tags": ["prasa"]
+    "tags": ["prasa", "inicjatywa"]
   },
   {
     "slug": "dobra-wiadomosc",
     "title": "Dobra wiadomość dla ul. Żeromskiego!",
     "date": "2026-08-11",
     "excerpt": "Dodatkowe środki z Miasta Otwock umożliwią realizację inwestycji na ul. Żeromskiego.",
-    "tags": ["inwestycja", "finansowanie"]
+    "tags": ["finansowanie"]
   },
   {
     "slug": "podsumowanie-zbiorki-pod-kosciolem",
     "title": "Podsumowanie zbiórki podpisów pod kościołem w Wólce Mlądzkiej!",
     "date": "2026-08-10",
     "excerpt": "Wczorajsza akcja zbierania podpisów pod petycją przy kościele Wniebowzięcia Najświętszej Maryi Panny w Otwocku przeszła nasze najśmielsze oczekiwania.",
-    "tags": ["petycja", "zbiórka"]
+    "tags": ["petycja", "inicjatywa"]
   },
   {
     "slug": "zbiorka-pod-kosciolem",
     "title": "Jesteśmy dziś pod kościołem w Wólce Mlądzkiej!",
     "date": "2026-08-09",
     "excerpt": "Zapraszamy po kolejnych Mszach.",
-    "tags": ["petycja", "zbiórka"]
+    "tags": ["petycja", "inicjatywa"]
   },
   {
     "slug": "analiza-segmentu-4",
     "title": "Opublikowaliśmy analizę segmentu 4",
     "date": "2026-08-05",
     "excerpt": "Publikujemy czwartą i ostatnią główną część projektu drogowego - odcinek od Żeromskiego 207 do 237.",
-    "tags": ["analiza", "projekt"]
+    "tags": ["projekt"]
   },
   {
     "slug": "analiza-segmentu-3",
     "title": "Analiza segmentu 3 już dostępna",
     "date": "2026-08-04",
     "excerpt": "Dzielimy się z Wami trzecią częścią projektu drogowego – tym razem odcinek od Żeromskiego 131 do 215.",
-    "tags": ["analiza", "projekt"]
+    "tags": ["projekt"]
   },
   {
     "slug": "analiza-segmentu-2",
     "title": "Analiza segmentu 2 już jest!",
     "date": "2026-08-04",
     "excerpt": "Zapraszamy do zapoznania się z drugą częścią projektu drogowego – tym razem odcinek od Żeromskiego 89 do 131.",
-    "tags": ["analiza", "projekt"]
+    "tags": ["projekt"]
   },
   {
     "slug": "analiza-segmentu-1",
     "title": "Analiza segmentu 1 opublikowana",
     "date": "2026-08-03",
     "excerpt": "Pierwszy odcinek projektu obejmuje fragment od skrzyżowania z ulicą Jałowcową do Żeromskiego 79A",
-    "tags": ["analiza", "projekt"]
+    "tags": ["projekt"]
   },
   {
     "slug": "glowne-zalozenia",
     "title": "Główne założenia projektu",
     "date": "2026-08-03",
     "excerpt": "Przedstawiamy główne założenia projektu przebudowy ul. Żeromskiego, które będą podstawą do dalszych analiz i konsultacji.",
-    "tags": ["analiza", "projekt", "założenia"]
+    "tags": ["projekt"]
   },
   {
     "slug": "inicjatywa-powstaje",
