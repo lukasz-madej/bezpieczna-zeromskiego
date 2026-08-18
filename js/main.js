5b553f4fcc2bd3a3d45f61d8859cf9f472aa0756
Lukasz Madej <lukasz.madej@gmail.com>
2026-08-18 09:42:03 +0200

    Fix bugs

--- js/main.js
+++ js/main.js
@@ -358,7 +358,7 @@ function updatePetycjaProgress(total, { animate = false } = {}) {
   }
 
   function tagHtml(tags) {
-    const colourMap = { petycja: 'petycja', inicjatywa: 'inicjatywa', projekt: 'projekt', przetarg: 'przetarg', analiza: 'analizy', działania: 'petycja', mieszkańcy: 'inicjatywa', powiat: 'przetarg' };
+    const colourMap = { petycja: 'petycja', inicjatywa: 'inicjatywa', projekt: 'projekt', przetarg: 'przetarg', analiza: 'analizy', działania: 'petycja', mieszkańcy: 'inicjatywa', powiat: 'przetarg', prasa: 'przetarg', finansowanie: 'projekt' };
     return (tags || []).map(t =>
       `<span class="news-tag news-tag-${colourMap[t] || ''}">${t}</span>`
     ).join('');
